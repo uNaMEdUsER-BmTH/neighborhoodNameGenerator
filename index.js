9030fda0c20e0f114ade4ec7d2a08f9894768bcf
uNaMEdUsER666 <thgeometrydash@gmail.com>
2024-09-16 13:41:00 +0000

    test

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const prefixes=["Sanctuary at",""];
-const names =["Alexander","Tilden","Emerald"];
-const suffixes = ["Pointe","Ridge","Place"];
+const prefixes=["Sanctuary at"];
+const names =["Alexander","Tilden","Emerald","Windermere","Wintermere"];
+const suffixes = ["Pointe","Ridge","Place","Knoll","Hill","Lake"];
 
-output = document.getElementById("name");
-generateButton = document.getElementById("generate");
-
+const output = document.getElementById("name");
+const generateButton = document.getElementById("generate");
+const neighborhood = document.getElementById("neighborhood")
 generateButton.addEventListener("click", generateName);
+
 function generateName() {
-console.log((Math.random()*names.length())+1);
-}
+    let name = Math.floor(Math.random()*names.length);
+    let prefix = Math.floor(Math.random()*prefixes.length);
+    let suffix = Math.floor(Math.random()*suffixes.length);
+    let textOutput = names.at(name) + " " + suffixes.at(suffix)
+    console.log(name + " " + suffix);
+    console.log(textOutput);
+    output.innerHTML = textOutput;
+    neighborhood.innerHTML = "Your Genereated Neighborhood Name is: ";
+    //console.log(prefix + " " + name + " " + suffix);
+    //console.log(prefixes.at(prefix) + " " + names.at(name) + " " + suffixes.at(suffix));
+    
+};
