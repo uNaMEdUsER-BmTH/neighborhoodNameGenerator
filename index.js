a6fdc7d48636c4540d69eb6cd6fd57f669a0aa44
uNaMEdUsER666 <thgeometrydash@gmail.com>
2024-09-16 18:07:05 +0000

    test 2

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const prefixes=["Sanctuary at"];
-const names =["Alexander","Tilden","Emerald","Windermere","Wintermere"];
-const suffixes = ["Pointe","Ridge","Place","Knoll","Hill","Lake"];
+const names =["Alexander","Tilden","Emerald","Windermere","Wintermere","Hamlin","Summer","Orange","Magnolia","Oak","Winter","Maple","Forest"];
+const suffixes = ["Pointe","Ridge","Place","Knoll","Hill","Lake","Grove","Park","Waters"];
 
 const output = document.getElementById("name");
 const generateButton = document.getElementById("generate");
-const neighborhood = document.getElementById("neighborhood")
+const neighborhood = document.getElementById("neighborhood");
+const list = document.getElementById("list");
 generateButton.addEventListener("click", generateName);
 
 function generateName() {
@@ -15,6 +16,7 @@ function generateName() {
     console.log(name + " " + suffix);
     console.log(textOutput);
     output.innerHTML = textOutput;
+    list.innerHTML += textOutput + "<br>";
     neighborhood.innerHTML = "Your Genereated Neighborhood Name is: ";
     //console.log(prefix + " " + name + " " + suffix);
     //console.log(prefixes.at(prefix) + " " + names.at(name) + " " + suffixes.at(suffix));
